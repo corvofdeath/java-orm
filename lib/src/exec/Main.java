ed1d1aa4a1ebdc6235ecd501ae143611d6b34b90
Filipe Santiago <porto.santiago@hotmail.com>
2018-06-09 11:33:41 -0300

    connection test

--- lib/src/exec/Main.java
+++ lib/src/exec/Main.java
@@ -1,4 +1,27 @@
 package exec;
 
+import orm.CreateDatabase;
+
+import java.sql.*;
+
 public class Main {
+
+    public static void main (String[] args) throws Exception {
+        try {
+
+            String serverName = "localhost:3306";
+            String mydatabase = "softsoft";
+            String url = "jdbc:mysql://" + serverName + "/" + "?autoReconnect=true&useSSL=false&useTimezone=true&serverTimezone=UTC";
+
+            String username = "root";
+            String password = "root";
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            CreateDatabase create = new CreateDatabase();
+            create.Create(connection, "teste2");
+        } catch (Exception e) {
+            throw e;
+        }
+
+    }
 }
--- lib/src/orm/CreateDatabase.java
+++ lib/src/orm/CreateDatabase.java
@@ -1,4 +1,23 @@
 package orm;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class CreateDatabase {
+
+    private String create = "CREATE DATABASE  IF NOT EXISTS ";
+
+    public void Create(Connection connection, String database) {
+
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(this.create + database);
+            System.out.println("Database created successfully...");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erro on create statment");
+        }
+    }
 }
